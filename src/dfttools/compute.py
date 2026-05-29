7fece2e45d5a55386359f9ab13e5c92bb7523f8c
shahnoor <shahnoor3pl@gmail.com>
2026-05-29 16:30:04 -0400

    compute dos, TDF and mu(T) using bolztrap2 and QE data-file-schema.xml and saves to file. use those files to compute thermopower

---
+++ src/dfttools/compute.py
@@ -0,0 +1,264 @@
+# Uses boltztrap to interpolates using raw DFT data and then generates files for DOS, TDF and mu(T)
+
+import copy
+import os.path
+
+import ase
+import ase.io
+import matplotlib.pylab as plt
+import numpy as np
+
+
+
+import BoltzTraP2.bandlib as BL
+import BoltzTraP2.dft as BTP
+import BoltzTraP2.io as IO
+from BoltzTraP2 import fite, serialization, sphere, units
+from BoltzTraP2.misc import ffloat
+
+
+import contextlib
+import copy
+import os.path
+import sys
+from pathlib import Path
+
+
+# -------------------------------------------------------------
+
+dft_data_dir = "./"
+Efermi_DFT = 0
+niter = 20
+bt2filnam = dft_data_dir + "Nb3S4_BLZTRP_m{}.bt2".format(niter)
+n_bins = 20_000
+signature = "Nb3S4_PBEsol_NCPP_QE_data"
+Temperatures = np.linspace(10.0, 500.0, num=50)
+
+NstarDFT = -126
+NstarList = [-126.1, -126.15]
+
+fig_out_dir = "May10m_{}_bins_{}".format(niter, n_bins)
+Path(fig_out_dir).mkdir(exist_ok=True)
+
+# -------------------------------------------------------------
+
+
+def load_interpolation():
+    """
+    
+    """
+    if os.path.isfile(bt2filnam):
+        print("Loading the precalculated results from", bt2filnam)
+        data, equivalences, coeffs, metadata = serialization.load_calculation(
+            bt2filnam
+        )
+        print("done")
+
+
+
+    else:
+        print("No pregenerated bt2 file found; performing a new interpolation")
+        data = BTP.DFTData(dft_data_dir)
+        equivalences = sphere.get_equivalences(
+            data.atoms, data.magmom, niter * len(data.kpoints)
+        )
+        print(
+            "There are",
+            len(equivalences),
+            "equivalence classes in the output grid",
+        )
+        coeffs = fite.fitde3D(data, equivalences)
+        serialization.save_calculation(
+            bt2filnam,
+            data,
+            equivalences,
+            coeffs,
+            serialization.gen_bt2_metadata(data, data.mommat is not None),
+        )
+    return data, equivalences, coeffs
+
+
+def mu_of_T_compute():
+
+    data, equivalences, coeffs = load_interpolation()
+
+    lattvec = data.get_lattvec()
+    eband, vvband, cband = fite.getBTPbands(
+        equivalences, coeffs, lattvec, curvature=False
+    )
+
+
+    Cepsilon, Cdos, Cvvdos, cdos = BL.BTPDOS(eband, vvband, npts=n_bins)
+
+
+    # 0.02 Ha = 0.54 eV
+    mur = np.linspace(-.02, .02, 5000) + Efermi_DFT
+
+
+    N, L0, L1, L2, Lm11 = BL.fermiintegrals(
+        Cepsilon, Cdos, Cvvdos, mur=mur, Tr=Temperatures
+    )
+
+
+
+
+    print("computing for Nstar=", Nstar)
+    fig, axes1 = plt.subplots(1, 2, dpi=150, figsize=(10,4))
+
+    ax = axes1[0]
+    # plt.plot(mur, N.T)
+    ax.plot(mur/units.eV, N[0], 'o', label="T={}".format(Temperatures[0]))
+    ax.plot(mur/units.eV, N[-1], 's', label="T={}".format(Temperatures[-1]))
+    ax.axvline(Efermi_DFT/units.eV, 0, 1, color='green', label=r"$E_F\, DFT$")
+    ax.axhline(-206, 0, 1, color='green')
+
+    mu_of_T = []
+    for i, Nstar in enumerate(NstarList):
+        ifermi_list = [np.nonzero(N[i]<= Nstar)[0][0] for i in range(len(Temperatures))]
+        Nvalues = [N[k, i] for k in range(len(Temperatures)) for i in ifermi_list]
+        mu_values = [mur[i] for i in ifermi_list]
+        mu_values = np.array(mu_values)
+        mu_of_T.append(mu_values)
+        # print(mu_values)
+
+        if i == 0:
+            ax.axhline(Nvalues[0], 0, 1, color='black')
+            ax.axhline(Nvalues[-1], 0, 1, color='black')
+            ax.axvline(mu_values[0]/units.eV, 0, 1, color='red')
+            ax.axvline(mu_values[-1]/units.eV, 0, 1, color='red')
+
+            ax.set_xlim(np.array([-0.7, 0.7]) + Efermi_DFT/units.eV)
+            ax.set_ylim(np.array([-1, 1]) + NstarDFT)
+            ax.legend()
+            # plt.xlabel(r"$\mu=\epsilon-\epsilon_{DFT} \, [Ha]$")
+            ax.set_xlabel(r"$\epsilon \, [eV]$")
+            ax.set_ylabel(r"$Electron\, Count,\ N$")
+
+
+        # ifermi = np.nonzero(mur >= Efermi_DFT)[0][0]
+        # print(mur[ifermi])
+
+
+            
+            x, y = Temperatures, (mu_values-Efermi_DFT)/units.eV * 1000
+            print("shift needed to EF (eV) = ", np.min(y))
+            # y[:2] = np.min(y)
+            coeff = np.polyfit(x, y, 3)
+            polynomial = np.poly1d(coeff)
+            # print(coeff)
+            ax = axes1[1]
+            ax.plot(x, y, label="Our DFT")
+            ax.plot(x, polynomial(x), label="Our DFT fit")
+            ax.set_ylabel(r"$\mu - E_F \, [m eV]$")
+            ax.set_xlabel(r"T(K)")
+            pass
+        pass
+    mu_of_T = np.array(mu_of_T)
+    Temperatures = np.array(Temperatures, shape=(-1,1))
+    print(mu_of_T.shape)
+    
+    combined = np.hstack((Temperatures, mu_of_T))
+
+
+    # Write to file
+    np.savetxt(dft_data_dir + signature + "_mu_of_T.csv", combined, fmt="%.6f", delimiter=",")
+
+
+    pass
+
+
+def dos_TDF_compute():
+
+    data, equivalences, coeffs = load_interpolation()
+
+    lattvec = data.get_lattvec()
+    eband, vvband, cband = fite.getBTPbands(
+        equivalences, coeffs, lattvec, curvature=False
+    )
+
+
+
+    
+    erange = np.array([-5,5])*units.eV + Efermi_DFT
+    Cepsilon, Cdos, Cvvdos, cdos = BL.BTPDOS(eband, vvband, npts=n_bins, erange=erange)
+
+
+    const = 1.0
+    Mvvdos = const * Cvvdos/Cdos
+    Mvvdos[np.isnan(Mvvdos)] = 0.0
+
+
+    # Unit conversion to eV
+    energy = (Cepsilon - Efermi_DFT)/units.eV
+    DOS = Cdos * units.eV
+    TDF_CRTA = Cvvdos
+    TDF_IDOS = Cvvdos / DOS
+
+
+    np.savetxt(dft_data_dir + signature + "_DOS.csv", np.c_[energy, DOS], 
+            delimiter=",",
+            header="#(E-E_F) [eV], DOS [eV^-1], {}bins in range [-5,5] eV, m={} interpolation. {}".format(n_bins, niter, signature))
+
+
+    np.savetxt(dft_data_dir + signature + "_TDF_CRTA.csv", np.c_[energy,TDF_CRTA[0,0,:],TDF_CRTA[1,1,:],TDF_CRTA[2,2,:]], 
+            delimiter=",",
+            header="#(E-E_F) [eV], TDF_xx, TDF_yy, TDF_zz, for CRTA, {}bins in range [-5,5] eV, m={} interpolation. {}".format(n_bins, niter, signature))
+
+    np.savetxt(dft_data_dir + signature + "_TDF_IDOS.csv", np.c_[energy,TDF_IDOS[0,0,:],TDF_IDOS[1,1,:],TDF_IDOS[2,2,:]], 
+            delimiter=",",
+            header="#(E-E_F) [eV], TDF_xx, TDF_yy, TDF_zz, for Inverse DOS, {}bins in range [-5,5] eV, m={} interpolation. {}".format(n_bins, niter, signature))
+
+
+
+
+
+    # Plot settings
+    xmin, xmax = -0.4, 0.4
+
+    fig, axes = plt.subplots(
+        4, 1,
+        figsize=(8, 10),
+        sharex=True,
+        gridspec_kw={"hspace": 0.05}
+    )
+
+    # DOS
+    axes[0].plot(energy, DOS, lw=1.5)
+    axes[0].set_ylabel("DOS")
+    axes[0].axvline(0, linestyle="--", linewidth=1)
+
+    # TDF_xx
+    axes[1].plot(energy, TDF_CRTA[0, 0,:], lw=1.5, label="CRTA")
+    axes[1].plot(energy, TDF_IDOS[0, 0,:], lw=1.5, label="IDOS")
+    axes[1].set_ylabel(r"TDF$_{xx}$")
+    axes[1].axvline(0, linestyle="--", linewidth=1)
+
+    # TDF_yy
+    axes[1].plot(energy, TDF_CRTA[1, 1, :], lw=1.5, label="CRTA")
+    axes[1].plot(energy, TDF_IDOS[1, 1, :], lw=1.5, label="IDOS")
+    axes[2].set_ylabel(r"TDF$_{yy}$")
+    axes[2].axvline(0, linestyle="--", linewidth=1)
+
+    # TDF_zz
+    axes[1].plot(energy, TDF_CRTA[2, 2, :], lw=1.5, label="CRTA")
+    axes[1].plot(energy, TDF_IDOS[2, 2, :], lw=1.5, label="IDOS")
+    axes[3].set_ylabel(r"TDF$_{zz}$")
+    axes[3].set_xlabel("Energy (eV)")
+    axes[3].axvline(0, linestyle="--", linewidth=1)
+
+    # Common x-range
+    axes[3].set_xlim(xmin, xmax)
+
+    plt.tight_layout()
+    plt.savefig("dos_TDF.png")
+
+
+
+
+if __name__ == "__main__":
+
+    dos_TDF_compute()
+    mu_of_T_compute()
+
+
+    pass
---
+++ src/dfttools/compute_TEP.py
@@ -0,0 +1,277 @@
+# Uses boltztrap to interpolates using raw DFT data and then generates DOS.
+
+import copy
+import os.path
+
+import ase
+import ase.io
+import matplotlib.pylab as plt
+import numpy as np
+
+
+
+import BoltzTraP2.bandlib as BL
+import BoltzTraP2.dft as BTP
+import BoltzTraP2.io as IO
+from BoltzTraP2 import fite, serialization, sphere, units
+from BoltzTraP2.misc import ffloat
+
+
+import contextlib
+import copy
+import os.path
+import sys
+from pathlib import Path
+
+
+
+# -------------------------------------------------------------
+
+dft_data_dir = "./"
+Efermi_DFT = 0
+niter = 20
+bt2filnam = dft_data_dir + "Nb3S4_BLZTRP_m{}.bt2".format(niter)
+n_bins = 20_000
+signature = "Nb3S4_PBEsol_NCPP_QE_data"
+Temperatures = np.linspace(10.0, 500.0, num=50)
+
+NstarDFT = -126
+NstarList = [-126.1, -126.15]
+
+
+
+
+
+
+fig_out_dir = "May10m_{}_bins_{}".format(niter, n_bins)
+Path(fig_out_dir).mkdir(exist_ok=True)
+
+bt2filnam = dft_data_dir + "Nb3S4_BLZTRP_m{}.bt2".format(niter)
+
+filename_DOS = dft_data_dir      + "Nb3S4_PBEsol_NCPP_QE_data24_DOS_m_{}_bins_{}.csv".format(niter, n_bins)
+filename_TDF_CRTA = dft_data_dir +  "Nb3S4_PBEsol_NCPP_QE_data24_TDF_CRTA_m_{}_bins_{}.csv".format(niter, n_bins)
+filename_TDF_IDOS = dft_data_dir +  "Nb3S4_PBEsol_NCPP_QE_data24_TDF_IDOS_m_{}_bins_{}.csv".format(niter, n_bins)
+
+
+# -------------------------------------------------------------
+
+
+from BoltzTraP2.units import *
+
+BOLTZMANN_EV = BOLTZMANN/units.eV
+print(BOLTZMANN_EV)
+
+def FD(E, mu, T):
+    kT =  T*BOLTZMANN_EV
+    return 1.0/(np.exp((E-mu)/kT) + 1.0)
+
+def dFD_dE(E, mu, T):
+    kT =  T*BOLTZMANN_EV
+    f = FD(E, mu, T)
+    return -f*(1-f)/kT
+
+def fermi_integral(E_values, TDF_values, T, mu):
+    dE = E_values[1]-E_values[0]
+    int0 = -TDF_values*dFD_dE(E_values, mu, T)
+    L0 = np.sum(int0)*dE
+    int1 = int0*(E_values-mu)
+    L1 = np.sum(int1)*dE
+    return L0, L1
+
+
+def shift_and_clip(E_values, deltaE, erange=(-0.4,0.4)):
+    x = E_values - deltaE
+    idx = np.logical_and(x <= erange[1], x >= erange[0])
+    x = x[idx]
+    return x, idx
+
+
+def get_thermopower(E_values, TDF_values, Tlist, mulist=None):
+    if mulist is None:
+        mulist = np.copy(Tlist)*0.0
+        pass
+
+    tmp = []
+    for i, T in enumerate(Tlist):
+        L0, L1 = fermi_integral(E_values, TDF_values, T, mulist[i])
+        tmp.append(L1/L0/(-T))
+
+    return np.array(tmp)
+
+
+def get_tau_from_dos(energy_, dos_, erange=(-5,5)):
+    """
+    Get scattering time tau from dos in a given range.
+
+    If there are zero's in the DOS, using L2 regularization we avoid that. Effect is that there could be unwanted peaks on
+    either side of the zero but that shouldn't change overall behavior of computed transport coeff.
+    
+    """
+    SCALE = 1e-2
+    if erange is not None:
+        idx = np.logical_and(energy_ >= erange[0], energy_ <= erange[1])
+        x = energy_[idx]
+        y = dos_[idx]
+    else:
+        x=energy_
+        y = dos_
+    
+    if abs(y.min() - 0) < 1e-3:
+        lam = SCALE * y.max()
+        tau = y/(y**2 + lam)
+        pass
+    else:
+        tau = 1/y
+        pass
+
+    tau[np.isnan(tau)] = 0.0
+
+    return x, tau
+
+
+
+
+def compute_Szz_Sxx():
+
+    energy, DOS = np.loadtxt(filename_DOS, delimiter=',').T
+
+    energy1, TDF_xx_CRTA, TDF_yy_CRTA, TDF_zz_CRTA = np.loadtxt(filename_TDF_CRTA, delimiter=',').T
+
+    energy2, TDF_xx_IDOS, TDF_yy_IDOS, TDF_zz_IDOS = np.loadtxt(filename_TDF_IDOS, delimiter=',').T
+
+    Tzzdata, Szzdata = np.loadtxt("../thermopower.txt").T
+    T1xxdata, S1xxdata = np.loadtxt("../thermopower.txt").T
+
+
+    for shift in range(-50, 60, 5):
+        Ep=shift/1000
+        x, idx = shift_and_clip(energy, Ep, erange=(-0.6, 0.6))
+        TDF_values = TDF_zz_IDOS[idx]
+        DOS_values = DOS[idx]
+
+        fig, axes = plt.subplots(2, 2, figsize=(24, 5), dpi=200)
+        axes = axes.flatten()
+
+
+        ax = axes[0]
+        ax.plot(energy, DOS, label="DOS")
+        ax.plot(x, DOS_values, label="DOS, shifted")
+        ax.set_xlabel("(E-E_F-{}meV ) (eV)".format(Ep*1000))
+        ax.axvline(0, 0, 10, color='red')
+        ax.set_xlim([-0.2, 0.2])
+        ax.set_ylim(0, 8)
+        ax.legend()
+
+
+        ax = axes[1]
+        ax.plot(energy, TDF_zz_IDOS, label="TDF")
+        ax.plot(x, TDF_values, label="TDF, shifted")
+        ax.plot(energy, -dFD_dE(energy, 0, 300)/30, 'k--', label="-df/dE(T=300K)/30")
+        ax.axvline(0, 0, 10, color='red')
+        ax.set_xlabel("(E-E_F-{}meV ) (eV)".format(Ep*1000))
+        ax.set_xlim([-0.2, 0.2])
+        ax.set_ylim(0, 0.7)
+        ax.legend()
+
+
+        ax = axes[2]
+        ax.plot(Tzzdata, Szzdata, 'o', label="Experiment", alpha=0.6)
+
+
+
+        
+        Tlist = np.linspace(10, 400, 20)
+        SofT = get_thermopower(x, TDF_values, Tlist)
+        ax.plot(Tlist, SofT*10**6, label=r"$\mu(T)=0$, Org.")
+        ax.set_ylim(-60, 20)
+        ax.set_xlabel(r"Temperature (K)")
+        ax.set_ylabel(r"Thermopwoer, $S_zz\, \mu V/K$")
+        ax.legend()
+
+
+
+        ax = axes[3]
+        ax.plot(T1xxdata, S1xxdata, 'o', label="Experiment", alpha=0.6)
+
+
+
+        
+        Tlist = np.linspace(10, 400, 20)
+        SofT = get_thermopower(x, TDF_values, Tlist)
+        ax.plot(Tlist, SofT*10**6, label=r"$\mu(T)=0$, Org.")
+        ax.set_ylim(-60, 20)
+        ax.set_xlabel(r"Temperature (K)")
+        ax.set_ylabel(r"Thermopwoer, $S_zz\, \mu V/K$")
+        ax.legend()
+
+        plt.savefig(fig_out_dir + "/Nb3S4_m_{}_bins_{}_Ep_{:.2f}meV.png".format(niter, n_bins, Ep*1000))
+    # break
+
+
+
+def compute_Szz():
+
+    energy, DOS = np.loadtxt(filename_DOS, delimiter=',').T
+
+    energy1, TDF_xx_CRTA, TDF_yy_CRTA, TDF_zz_CRTA = np.loadtxt(filename_TDF_CRTA, delimiter=',').T
+
+    energy2, TDF_xx_IDOS, TDF_yy_IDOS, TDF_zz_IDOS = np.loadtxt(filename_TDF_IDOS, delimiter=',').T
+
+    Tdata, Sdata = np.loadtxt("../thermopower.txt").T
+    
+
+
+    for shift in range(-10, 60, 1):
+        Ep=shift/1000
+        x, idx = shift_and_clip(energy, Ep, erange=(-0.6, 0.6))
+        TDF_values = TDF_zz_IDOS[idx]
+        DOS_values = DOS[idx]
+
+        fig, axes = plt.subplots(1, 3, figsize=(24, 5), dpi=200)
+        axes = axes.flatten()
+
+
+        ax = axes[0]
+        ax.plot(energy, DOS, label="DOS")
+        ax.plot(x, DOS_values, label="DOS, shifted")
+        ax.set_xlabel("(E-E_F-{}meV ) (eV)".format(Ep*1000))
+        ax.axvline(0, 0, 10, color='red')
+        ax.set_xlim([-0.2, 0.2])
+        ax.set_ylim(0, 8)
+        ax.legend()
+
+
+        ax = axes[1]
+        ax.plot(energy, TDF_zz_IDOS, label="TDF")
+        ax.plot(x, TDF_values, label="TDF, shifted")
+        ax.plot(energy, -dFD_dE(energy, 0, 300)/30, 'k--', label="-df/dE(T=300K)/30")
+        ax.axvline(0, 0, 10, color='red')
+        ax.set_xlabel("(E-E_F-{}meV ) (eV)".format(Ep*1000))
+        ax.set_xlim([-0.2, 0.2])
+        ax.set_ylim(0, 0.7)
+        ax.legend()
+
+
+        ax = axes[2]
+        ax.plot(Tdata, Sdata, 'o', label="Experiment", alpha=0.6)
+
+
+
+        
+        Tlist = np.linspace(10, 400, 20)
+        SofT = get_thermopower(x, TDF_values, Tlist)
+        ax.plot(Tlist, SofT*10**6, label=r"$\mu(T)=0$, Org.")
+        ax.set_ylim(-60, 20)
+        ax.set_xlabel(r"Temperature (K)")
+        ax.set_ylabel(r"Thermopwoer, $\mu V/K$")
+        ax.legend()
+
+        plt.savefig(fig_out_dir + "/Nb3S4_m_{}_bins_{}_Ep_{:.2f}meV.png".format(niter, n_bins, Ep*1000))
+    # break
+
+
+
+if __name__ == "__main__":
+    compute_Szz()
+    # plot()
+    
